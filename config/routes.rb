8de44d9e63f9f7a1e8ea6229f5a07ef051772ea0
loqimean <vanuha277@gmail.com>
2022-10-23 22:19:26 +0300

    reproduce turbo bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'some_url', to: 'home#some_url'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
